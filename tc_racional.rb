94edd6fecae903d691f64c7cbf8a9db6c6eee68d
alu0100537154 <alu0100537154@ull.edu.es>
2013-10-18 12:24:44 +0100

    practica5acabada

--- tc_racional.rb
+++ tc_racional.rb
@@ -8,15 +8,15 @@ class Test_Fraccion < Test::Unit::TestCase
         
         def setup
 			@numero = Fraccion.new(1,10)
-			@numero1 = Fraccion.new(1,0)
+			@numero1 = Fraccion.new(1,20)
 		end
         
         def test_1
         
-                assert_equal("1/10", @numero.suma(@numero1).to_s) 
-                assert_equal("0", @numero.resta(@numero1).to_s) 
-                assert_equal("1/10", @numero.multiplicacion(@numero1).to_s) 
-                assert_equal("1", @numero.division(@numero1).to_s) 
+                assert_equal("3/20", @numero.suma(@numero1).to_s) 
+                assert_equal("1/20", @numero.resta(@numero1).to_s) 
+                assert_equal("1/200", @numero.multiplicacion(@numero1).to_s) 
+                assert_equal("2", @numero.division(@numero1).to_s) 
                 
         end
 
