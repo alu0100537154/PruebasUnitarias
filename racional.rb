5d0fc6c44375ca98e5cb38550b5996f6d2765c5d
alu0100537154 <alu0100537154@ull.edu.es>
2013-10-18 12:18:22 +0100

    practica5

--- racional.rb
+++ racional.rb
@@ -3,4 +3,65 @@
 require "./gcd.rb"
 
 class Fraccion
+
+	attr_reader :numerador, :denominador #Numerador y denominador
+	
+	def initialize(nume,deno)
+		mcd = gcd(nume,deno)
+		@numerador = nume / mcd
+		@denominador =deno / mcd
+	end
+	
+	
+	def to_s
+		if @numerador == 0 
+			"0"
+		elsif @denominador == 1 
+			"#{@numerador}"	
+		elsif @numerador == @denominador 
+			"1"
+		else
+			"#{@numerador}/#{@denominador}"
+		end
+	end
+	
+	
+	def + (other)
+		Fraccion.new(@numerador * other.denominador + other.numerador * @denominador, @denominador * other.denominador)
+	end
+	
+	
+	def - (other)
+		Fraccion.new(@numerador * other.denominador - other.numerador * @denominador, @denominador * other.denominador)
+	end
+	
+	
+	def * (other)
+		Fraccion.new(@numerador * other.numerador, @denominador *other.denominador)
+	end
+		
+       
+	def / (other)
+		Fraccion.new(@numerador * other.denominador, @denominador * other.numerador)
+	end
 end
+
+
+
+
+a = Fraccion.new(1, 10)
+b = Fraccion.new(1, 20)
+
+print "Valor de a = "
+puts a
+print "Valor de b = "
+puts b
+
+print "La suma es = "
+puts a.+(b)
+print "La resta es = "
+puts a.-(b)
+print "La multiplicacion es = "
+puts a.*(b)
+print "La division es = "
+puts a./(b)
--- tc_racional.rb
+++ tc_racional.rb
@@ -3,6 +3,21 @@
 require "./racional.rb"
 require "test/unit"
 
+
 class Test_Fraccion < Test::Unit::TestCase
+        
+        def setup
+			@numero = Fraccion.new(1,10)
+			@numero1 = Fraccion.new(1,0)
+		end
+        
+        def test_1
+        
+                assert_equal("1/10", @numero.suma(@numero1).to_s) 
+                assert_equal("0", @numero.resta(@numero1).to_s) 
+                assert_equal("1/10", @numero.multiplicacion(@numero1).to_s) 
+                assert_equal("1", @numero.division(@numero1).to_s) 
+                
+        end
 
 end
